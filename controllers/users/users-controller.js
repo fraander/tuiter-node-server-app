fb6d49777fb3d7c6bc45919d70e8059cfe792cf5
fraander <57777918+fraander@users.noreply.github.com>
2023-03-14 14:43:28 -0400

    finish 3

--- controllers/users/users-controller.js
+++ controllers/users/users-controller.js
@@ -1,7 +1,28 @@
 import people from './users.js'
+
 let users = people
 
-const UserController = (app) => app.get('/api/users', findUsers)
-const findUsers = (req, res) => res.json(users)
+const UserController = (app) => {
+    app.get('/api/users', findUsers);
+    app.get('/api/users/:uid', findUserById);
+}
+
+const findUsers = (req, res) => {
+    const type = req.query.type
+
+    if (type) {
+        res.json(users.filter(u => u.type === type))
+    } else {
+        res.json(users)
+    }
+}
+
+const findUserById = (req, res) => {
+    const userId = req.params.uid;
+    const user = users
+        .find(u => u._id === userId);
+    res.json(user);
+}
+
 
 export default UserController
