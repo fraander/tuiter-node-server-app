4b7aeeb2bd98f6b3d408bb83df0a595ce72a7a7a
fraander <57777918+fraander@users.noreply.github.com>
2023-03-14 19:34:08 -0400

    section 5

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express from 'express'
+import TuitsController from "./controllers/tuits/tuits-controller.js"
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
 
 const app = express();
 app.use(express.json())
+TuitsController(app)
 HelloController(app)
 UsersController(app)
 app.listen(4000)
