ae12a9e9012d52da22eefeea935d8c6c8995ceb6
fraander <57777918+fraander@users.noreply.github.com>
2023-03-16 10:53:32 -0400

    update server

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express'
+import cors from 'cors'
+
 import TuitsController from "./controllers/tuits/tuits-controller.js"
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
 
 const app = express();
 app.use(express.json())
+app.use(cors())
+
 TuitsController(app)
 HelloController(app)
 UsersController(app)
-app.listen(4000)
+app.listen(process.env.PORT || 4000)
