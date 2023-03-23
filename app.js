20df9dcd24d0d4f7151327f04f7f3ce9a56ca0b3
fraander <57777918+fraander@users.noreply.github.com>
2023-03-23 11:10:44 -0400

    update connection string

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ import TuitsController from "./controllers/tuits/tuits-controller.js"
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
 
-mongoose.connect('mongodb://localhost:27017/tuiter')
+const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
+
+mongoose.connect(CONNECTION_STRING)
 
 const app = express();
 app.use(express.json())
