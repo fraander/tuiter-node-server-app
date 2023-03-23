795f8b9b11e53d841d468d24ea2f981e2bedd110
fraander <57777918+fraander@users.noreply.github.com>
2023-03-23 13:09:52 -0400

    update debug endpoint

--- controllers/hello-controller.js
+++ controllers/hello-controller.js
@@ -1,7 +1,10 @@
+import mongoose from "mongoose";
+
 const HelloController = (app) => {
     app.get('/hello', (req, res) => res.send('Life is good!'));
     app.get('/', (req, res) => res.send('Welcome to Full Stack Development'))
     app.get('/api', (req, res) => res.send(process.env.REACT_APP_DB_CONNECTION_STRING))
+    app.get('/api/connection', mongoose.connection)
 }
 
 export default HelloController
