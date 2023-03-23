188f49553ab669274dc0d00eebca0addf8100c91
fraander <57777918+fraander@users.noreply.github.com>
2023-03-23 12:44:23 -0400

    troubleshooting endpoint

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ import TuitsController from "./controllers/tuits/tuits-controller.js"
 import HelloController from "./controllers/hello-controller.js";
 import UsersController from "./controllers/users/users-controller.js";
 
-const CONNECTION_STRING = process.env.REACT_ENV_DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
+const CONNECTION_STRING = process.env.REACT_APP_DB_CONNECTION_STRING || 'mongodb://localhost:27017/tuiter'
 
 mongoose.connect(CONNECTION_STRING)
 
--- controllers/hello-controller.js
+++ controllers/hello-controller.js
@@ -1,6 +1,7 @@
 const HelloController = (app) => {
     app.get('/hello', (req, res) => res.send('Life is good!'));
     app.get('/', (req, res) => res.send('Welcome to Full Stack Development'))
+    app.get('/api', (req, res) => res.send(process.env.REACT_APP_DB_CONNECTION_STRING))
 }
 
 export default HelloController
